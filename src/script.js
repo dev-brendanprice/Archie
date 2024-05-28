56e1d7a1811b5dc64d7b1750cb947cd7caed1fc3
Brendan Price <56489848+dev-brendanprice@users.noreply.github.com>
2024-05-28 03:48:41 +0100

    Add bug fixes and improvements

--- src/script.js
+++ src/script.js
@@ -1,19 +1,16 @@
 import {get, set} from 'idb-keyval';
 import formatString from './helpers/formatString';
 
+window.scrollTo(0, 0); // Lazy load-scroll fix
 let locale = navigator.language.split('-')[0];
 let sb = document.getElementById('sbar');
 
-
-// Store list of results, ignore duplicates
-let searchList = [];
-
-// Store formatteda and un-formatted list of weapons from definitions
-let formattedWeaponsList = [];
-let weaponsList = [];
+// Store formatted and un-formatted list of weapons from definitions
+let formattedWeaponsList = []; // [Deprecate this]
+let weaponsList = []; // [Deprecate this]
 
 // Fetch definitions
-fetchDefinitions();
+fetchDefinitions(); // [Deprecate this]
 
 
 
@@ -89,17 +86,9 @@ async function makeList() {
 
 // Fetch articles via searchText
 let searchByText = async function(searchText) {
-
-    searchText = formatString(searchText);
-
-    // Find weapon name in definitions
-    let index = formattedWeaponsList.indexOf(searchText);
-    let newSearchText = weaponsList[index];
-    console.log(`${searchText} : ${newSearchText}`);
-
-
+    console.log('input:',searchText);
     // Set config
-    let url = `https://www.bungie.net/Platform/Content/Search/${locale}/?searchtext=${newSearchText}&ctype=news`;
+    let url = `https://www.bungie.net/Platform/Content/Search/${locale}/?searchtext=${searchText}&ctype=help news`;
     let config = {
         method: "GET",
         headers: {
@@ -113,87 +102,134 @@ let searchByText = async function(searchText) {
     });
     
     // Ignore mismatched origin (lazy fix) and empty field
-    if (newSearchText !== '' && response.ErrorCode !== 2107) {
+    if (searchText !== '' && response.ErrorCode !== 2107) {
         return response.json();
     };
 };
 
-// "Use" response
-let parseResponse = async function(data) {
-    
-    // Ignore invalid responses
-    let results = data.Response.results;
-    if (results.length > 0 && results[0].cmsPath) {
+// Use response
+let parseResponse = function(data) {
 
-        document.getElementById('results').innerHTML = ''; // check for recurring search term
-        
-        let index = 0;
-        window.localStorage.setItem('results', JSON.stringify({})); // init
+    // Localised vars
+    const response = data.Response.results;
+    const resultsArr = {};
 
-        // Loop over results, add to DOM
-        for (let result of results) {
-            
-            // Ignore duplicate results
-            let title = `${result.cmsPath} - ${result.creationDate}`;
-            if (!searchList.includes(title)) {
-
-                // Create, append to DOM
-                let resdiv = document.createElement('div');
-                resdiv.innerHTML = title;
-                resdiv.setAttribute('data-index', index);
-                document.getElementById('results').appendChild(resdiv);
-
-                // Save result to localstorage obj
-                let obj = JSON.parse(window.localStorage.getItem('results'));
-                obj[index] = result.properties.Content;
-                console.log(result);
-                window.localStorage.setItem('results', JSON.stringify(obj));
-
-                // Add to list
-                searchList.push(title);
-                index++;
-                console.log(title);
-
-                // Use attribute to display content
-                resdiv.addEventListener('click', () => {readAttr(resdiv, data)});
-            };
-        };
+    // Create array of results
+    for (let result of response) {
+        resultsArr[result.properties.Title] = result.properties.Content;
     };
+
+    // Counter, init & LocalStorage
+    let i = 0;
+    let localStorageObj = {};
+    window.localStorage.setItem('results', JSON.stringify({}));
+    document.getElementById('results').innerHTML = ''; // Remove current DOM list
+
+    // Map over array and create DOM elements for each
+    Object.entries(resultsArr).forEach((item) => {
+        
+        // Create new div with attr, add to DOM & LocalStorage
+        const div = document.createElement('div');
+        localStorageObj[i] = item[1];
+        div.innerHTML = item[0];
+        div.setAttribute('data-index', i);
+        div.addEventListener('click', () => {readAttr(div, data)});
+        document.getElementById('results').appendChild(div);
+        i++;
+    });
+
+    // Add results to LocalStorage
+    window.localStorage.setItem('results', JSON.stringify(localStorageObj));
 };
 
 // Read attribute from element onclick
 function readAttr(div, data) {
+
+    // DOM content
+    document.getElementById('singlerescon').style.display = 'block';
     
     // Get stored results and selected-entry index
     let query = data.Response.query.searchText;
-    console.log(query);
-    let storedresults = JSON.parse(window.localStorage.getItem('results'));
+    let storedResults = JSON.parse(window.localStorage.getItem('results'));
     let index = div.getAttribute('data-index');
+    console.log('return search:',data.Response.query.searchText);
 
     // Highlight search term in content
     let regex = new RegExp(query, 'gi');
-    let content = storedresults[index].replace(/(<mark class="highlight">|<\/mark>)/gim, '');
+    let content = storedResults[index].replace(/(<mark class="highlight">|<\/mark>)/gim, '');
 
-    // -- and change DOM content
+    // Change DOM content
     document.getElementById('mid').style.display = 'none';
     document.getElementById('titleresult').innerHTML = `Viewing: ${div.innerHTML}`;
     document.getElementById('singleresult').innerHTML = content.replace(regex, '<mark class="highlight">$&</mark>');
 
-    // Scroll to highlighted word (unsure when multiple)
-    window.scrollBy(0, document.evaluate(`//*[text()[contains(., '${query}')]][last()]`, document.body).iterateNext().getBoundingClientRect().top);
+    // Scroll to matching query
+    try {
+
+        query = query.toLowerCase();
+
+        // Search for query in document using XPath
+        const xpathResult = document.evaluate(
+            `//*[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '${query}')]`,
+            document.body,
+            null,
+            XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+            null
+        );
+
+        // Find all matching strings
+        let matches = [];
+        for (let i=0; i<xpathResult.snapshotLength; i++) {
+            matches.push(xpathResult.snapshotItem(i));
+        };
+        console.log(matches, matches.length);
+        
+        // Scroll to result with offset
+        const rectPos = matches[0].getBoundingClientRect().top;
+        const offsetPos = rectPos - 200;
+
+        window.scrollTo({ top: offsetPos, behavior: 'smooth' });
+    
+    }
+    catch (e) {
+        console.error(e);
+    };
+};
+
+
+// Configure debounce
+let debounceTimer;
+const debounce = (callback, time) => {
+    window.clearTimeout(debounceTimer);
+    debounceTimer = window.setTimeout(callback, time);
 };
 
 
 // Retro-active event search (on key press it searches)
-sb.addEventListener('keyup', (event) => {
+sb.addEventListener('keyup', async (event) => {
 
-    // Exclude modifier keys from event
+    // Turn on throbber
     if (event.code !== 'Enter' && event.code !== 'ShiftLeft' && event.code !== 'ControlLeft' && event.code !== 'CapsLock' && event.code !== 'Space') {
-        searchByText(sb.value).then((data) => { // Fetch URL
-
-            if (data) { // Ignore falsy return
-                parseResponse(data);
-            };
-        });
+        document.getElementById('loadingSpinner').style.display = 'flex';
     };
+
+    // Localised function
+    async function search() {
+
+        // Exclude modifier keys from event
+        if (event.code !== 'Enter' && event.code !== 'ShiftLeft' && event.code !== 'ControlLeft' && event.code !== 'CapsLock' && event.code !== 'Space') {
+            await searchByText(sb.value).then((data) => { // Fetch URL
+
+                if (data) { // Ignore falsy return
+                    parseResponse(data);
+                };
+            })
+            .then(() => {
+                document.getElementById('loadingSpinner').style.display = 'none';
+            });
+        };
+    };
+
+    // Use search as a callback on debounce function
+    debounce(search, 500);
 });
